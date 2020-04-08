89b79ae9c01e3cce9ef76d0359c3b9d89fe0642a
Gian Franco <gianfrancogv@gmail.com>
2020-04-08 13:05:54 -0300

    Cotizando seguro

---
+++ js/app.js
@@ -0,0 +1,105 @@
+//Cotizador Constructor
+function Seguro (marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+Seguro.prototype.cotizarSeguro = function () {
+    /*
+        1 = Americano: 1.15
+        2 = Asiático: 1.05
+        3 = Europeo: 1.35
+    */
+
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+        case '2':
+            cantidad = base * 1.05;
+            break;
+        case '3':
+            cantidad = base * 1.35;
+            break;
+    }
+
+    const diferencia = new Date().getFullYear() - this.anio;
+
+    //Cada año de diferencia hay que reducir 3% el valor del seguro
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+    
+    /* TIPOS DE SEGURO:
+    Basico: se multiplica por 30% mas
+    Completo: se multiplica por 50% mas
+    */
+
+    if (this.tipo === 'basico') {
+        cantidad *= 1.3;
+    } else {
+        cantidad *= 1.5;
+    }
+}
+
+//Constructor Interfaz (Todo lo que se muestra)
+function Interfaz () {
+
+}
+
+//Mensaje que se imprime en el HTML
+Interfaz.prototype.mostrarError = function(mensaje, tipo) {
+    const div = document.createElement('div');
+    if (tipo === 'error') {
+        div.classList.add('mensaje', 'error');
+    } else {
+        div.classList.add('mensaje', 'correcto');
+    }
+    div.innerHTML = `${mensaje}`;
+    formulario.insertBefore(div, document.querySelector('.form-group'));
+
+    setTimeout(function() {
+        document.querySelector('.mensaje').remove();
+    }, 3000)
+}
+
+//Event Listeners
+const formulario = document.getElementById('cotizar-seguro');
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    //Leer el Index de la marca seleccionada
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+    //Leer el anio seleccionado
+    const anio = document.getElementById('anio');
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+    //Lee el valor del radio button
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    //Crear instancia de Interfaz
+    const interfaz = new Interfaz();
+
+    //Revisamos que los campos no esten vacíos
+    if (marcaSeleccionada === '' || anioSeleccionado === '' || tipo === '') {
+        interfaz.mostrarError('Faltan datos, revisa el formulario y prueba de nuevo', 'error');
+    } else {
+        const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+        //Cotizar seguro
+        const cantidad = seguro.cotizarSeguro();
+    }
+});
+
+//Fecha máxima y mínima
+const max = new Date().getFullYear();
+const min = max - 20;
+
+//Bucle para agregar años a las opciones
+const selectAnios = document.getElementById('anio');
+for (let i = max; i > min; i--) {
+    let option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    selectAnios.appendChild(option);
+}
